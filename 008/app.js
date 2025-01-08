9ef51e3e6d2e18038ac8c547620df8dfca033b66
A-Kija <arvydas.kijakauskas@gmail.com>
2025-01-08 11:01:42 +0200

    008

---
+++ 008/app.js
@@ -0,0 +1,34 @@
+console.log('Labas, Bebras!');
+
+let skaicius1 = 5; //tipas - number
+
+console.log(skaicius1);
+
+let pasisveikinimas = 'Labas, Bebras!'; //tipas - string
+
+console.log(pasisveikinimas);
+
+let kazkas = '30'; //tipas - string
+
+let htmlGabalas = '<a href="http://www.delfi.lt">Delfi</a>'; //tipas - string
+
+let skaicius2 = 5.5; //tipas - number
+
+let skaicius3 = skaicius1 + skaicius2; //tipas - number
+
+console.log(skaicius3);
+
+let kazkas2 = kazkas + skaicius1;
+
+console.log(kazkas2);
+
+let z1 = 'Be';
+let z2 = 'bras';
+
+let z3 = z1 + z2;
+
+console.log(z3);
+
+let sk = 0.3 + 0.6;
+
+console.log(sk);
