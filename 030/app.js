2bc24eeab8b5f67ac2436c1c1fb9279e788ff494
A-Kija <arvydas.kijakauskas@gmail.com>
2025-02-25 11:00:10 +0200

    030

---
+++ 030/app.js
@@ -0,0 +1,13 @@
+console.log('*** tai yra 3 žvaigždutės ***');
+console.log(87 + 14);
+console.log(87 - 14);
+console.log([87, 14, '87', 15]);
+
+class A{
+    constructor() {
+        this.A = 1;
+        this.B = 2;
+    }
+}
+
+console.log(new A());
