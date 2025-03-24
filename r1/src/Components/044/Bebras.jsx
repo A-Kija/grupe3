11c61b1d02adbf6d08cf5b63ce6f524ffb1c4bb0
A-Kija <arvydas.kijakauskas@gmail.com>
2025-03-24 13:46:22 +0200

    044

---
+++ r1/src/Components/044/Bebras.jsx
@@ -0,0 +1,17 @@
+import './Bebras.css';
+import Kvadratas from './Kvadratas';
+
+export default function Bebras() {
+
+
+    return (
+
+        <>
+            <Kvadratas />
+            <h2>BEBRAS</h2>
+            <span>upinis</span>
+        </>
+
+
+    );
+}
---
+++ r1/src/Components/044/Kvadratas.jsx
@@ -0,0 +1,9 @@
+import './kvadratas.css';
+
+export default function Kvadratas() {
+
+
+    return (
+        <div className="kvadratas"></div>
+    );
+}
